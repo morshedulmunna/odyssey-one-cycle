b7d1d81b6d55c4bd987de65eec91df66b54d906f
morshedulmunna <morshedulmunna1@gmail.com>
2022-04-03 23:27:29 +0600

    Header Part Done

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,24 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header__main">
+        <div className="wrapper">
+          <h1>
+            Odyssey <span>ONE</span>
+          </h1>
+          <p>
+            Available Best Electric Bike price In Bangladesh. Here we have
+            sorted out top-ranked E bike, Electric Scooter, and E-mopeds
+            currently available in the market from the various brands operating
+            in Bangladesh.
+          </p>
+          <button>Buy One</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
