82cdefb2edbe987438f7ed56263e8f012c6d38a3
morshedulmunna <morshedulmunna1@gmail.com>
2022-04-04 19:08:03 +0600

    Data Show and Responsive reChat

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -16,6 +16,7 @@ import {
   Tooltip,
   Bar,
   Treemap,
+  ResponsiveContainer,
 } from "recharts";
 const data = [
   {
@@ -100,66 +101,78 @@ const Dashboard = () => {
           <div className="chart-wrapper">
             <div className="chat_section1">
               <div className="chart-1">
-                <RadarChart
-                  outerRadius={150}
-                  width={600}
-                  height={350}
-                  data={data}
-                >
-                  <PolarGrid />
-                  <PolarAngleAxis dataKey="month" />
-                  <PolarRadiusAxis angle={20} domain={[0, 10]} />
-                  <Radar
-                    name="Sell"
-                    dataKey="sell"
-                    stroke="#51e8ef"
-                    fill="#51e8ef"
-                    fillOpacity={0.6}
-                  />
-                  <Radar
-                    name="Revenue"
-                    dataKey="revenue"
-                    stroke="#55F80F"
-                    fill="#55F80F"
-                    fillOpacity={0.6}
-                  />
-                  <Radar
-                    name="Investment"
-                    dataKey="investment"
-                    stroke="#0121FA"
-                    fill="#0121FA"
-                    fillOpacity={0.6}
-                  />
-                  <Tooltip />
-                  <Legend />
-                </RadarChart>
+                <ResponsiveContainer>
+                  <RadarChart outerRadius={100} data={data}>
+                    <PolarGrid />
+                    <PolarAngleAxis dataKey="month" />
+                    <PolarRadiusAxis angle={20} domain={[0, 10]} />
+                    <Radar
+                      name="Sell"
+                      dataKey="sell"
+                      stroke="#0121FA"
+                      fill="#0121FA"
+                      fillOpacity={0.6}
+                    />
+                    <Radar
+                      name="Revenue"
+                      dataKey="revenue"
+                      stroke="#55F80F"
+                      fill="#55F80F"
+                      fillOpacity={0.6}
+                    />
+                    <Radar
+                      name="Investment"
+                      dataKey="investment"
+                      stroke="#51e8ef"
+                      fill="#51e8ef"
+                      fillOpacity={0.6}
+                    />
+                    <Tooltip />
+                    <Legend />
+                  </RadarChart>
+                </ResponsiveContainer>
               </div>
               {/* Chart-2 */}
               <div className="chart-2">
-                <BarChart width={600} height={350} data={data}>
-                  <CartesianGrid strokeDasharray="1 7" />
-                  <XAxis dataKey="month" />
-                  <YAxis />
-                  <Tooltip />
-                  <Legend />
-                  <Bar dataKey="investment" fill="#51e8ef" />
-                  <Bar dataKey="sell" fill="#55F80F" />
-                  <Bar dataKey="revenue" fill="#0121FA" />
-                </BarChart>
+                <ResponsiveContainer>
+                  <BarChart data={data}>
+                    <CartesianGrid strokeDasharray="1 7" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="investment" fill="#51e8ef" />
+                    <Bar dataKey="sell" fill="#55F80F" />
+                    <Bar dataKey="revenue" fill="#0121FA" />
+                  </BarChart>
+                </ResponsiveContainer>
               </div>
             </div>
             <div className="chat_section2">
               <h3>Monthly Investment Data</h3>
-              <LineChart width={1230} height={400} data={data} margin={"auto"}>
-                <CartesianGrid strokeDasharray="1 3" />
-                <XAxis dataKey="month" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="investment" stroke="#51e8ef" />
-                <Line type="monotone" dataKey="sell" stroke="#55F80F" />
-                <Line type="monotone" dataKey="revenue" stroke="#0121FA" />
-              </LineChart>
+              <div className="lineChat">
+                <ResponsiveContainer>
+                  <LineChart
+                    width={1230}
+                    height={400}
+                    data={data}
+                    margin={"auto"}
+                  >
+                    <CartesianGrid strokeDasharray="1 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line
+                      type="monotone"
+                      dataKey="investment"
+                      stroke="#51e8ef"
+                    />
+                    <Line type="monotone" dataKey="sell" stroke="#55F80F" />
+                    <Line type="monotone" dataKey="revenue" stroke="#0121FA" />
+                  </LineChart>
+                </ResponsiveContainer>
+              </div>
             </div>
           </div>
         </div>
