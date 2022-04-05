448c456812fb3a8419ddc67f876f35890daa879e
morshedulmunna <morshedulmunna1@gmail.com>
2022-04-05 10:33:28 +0600

    error 404 page Design

--- src/component/error404/Error404.jsx
+++ src/component/error404/Error404.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "./Error404.css";
 const Error404 = () => {
   return (
-    <div>
-      <h1>Hello 404</h1>
+    <div className="Error">
+      <h1 id="forOfor">404</h1>
+      <div class="cloak__wrapper">
+        <div class="cloak__container">
+          <div class="cloak"></div>
+        </div>
+      </div>
+      <div class="info">
+        <h2>We can't find that page</h2>
+        <p>
+          We're fairly sure that page used to be here, but seems to have gone
+          missing. We do apologise on it's behalf.
+        </p>
+        <Link id="homePAge" to="./">
+          {" "}
+          Home Page{" "}
+        </Link>
+      </div>
     </div>
   );
 };
